41f78f513827faa60e01759aca071cbe9572e0ef
ushepeleva-creator <u.shepeleva@g.nsu.ru>
2026-05-21 14:25:38 +0300

    database.py

--- database.py
+++ database.py
@@ -1,23 +1,30 @@
 import json
 import os
+
 DATA_FILE = 'data.json'
-DEFAULT_DATA={
-    'clients':[
-        {'id':1,'name':'Oksana','phone':'8913'}
+DEFAULT_DATA = {
+    'clients': [
+        {'id': 1, 'name': 'Oksana', 'phone': '8913'}
     ]
 }
+
+
 def load_data():
     if not os.path.exists(DATA_FILE):
         save_data(DEFAULT_DATA)
         return DEFAULT_DATA
-    with open(DATA_FILE,'r', encoding='utf-8') as f:
+    with open(DATA_FILE, 'r', encoding='utf-8') as f:
         return json.load(f)
+
+
 def save_data(data):
-    with open(DATA_FILE,'w', encoding='utf-8') as f:
-        json.dump(data,f)
+    with open(DATA_FILE, 'w', encoding='utf-8') as f:
+        json.dump(data, f)
+
+
 if __name__ == '__main__':
-    data=load_data()
+    data = load_data()
     print(data)
-    data['clients'].append({'id':2,'name':'Kris','phone':'8929'})
+    data['clients'].append({'id': 2, 'name': 'Kris', 'phone': '8929'})
     save_data(data)
     print(data)
